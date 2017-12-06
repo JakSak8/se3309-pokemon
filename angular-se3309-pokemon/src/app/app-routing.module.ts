ef2e22c7604713207d46bec3ec4e84e1ffa1c533
Jake Sakon <jsakon@uwo.ca>
2017-12-06 16:52:22 -0500

    fixing the git hub

--- angular-se3309-pokemon/src/app/app-routing.module.ts
+++ angular-se3309-pokemon/src/app/app-routing.module.ts
@@ -4,21 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
-<<<<<<< HEAD
 import { TeamComponent } from './team/team.component';
-=======
 import { HomeComponent } from './home/home.component';
->>>>>>> 37a91a3821ff8dd63cc6e4b37c5d74a89679e52c
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent },
   { path: 'pokemon', component: PokemonComponent },
-<<<<<<< HEAD
   { path: 'team', component: TeamComponent },
-=======
   { path: 'home', component: HomeComponent}
->>>>>>> 37a91a3821ff8dd63cc6e4b37c5d74a89679e52c
 ]
 
 @NgModule({
